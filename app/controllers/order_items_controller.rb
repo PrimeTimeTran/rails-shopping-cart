9d94ceadd82d109730cb364814da0972763bd06e
Loi Tran <lvt15@my.fsu.edu>
2017-06-16 19:01:46 +0700

    corrected error where the cart had multiple items

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,33 @@
+class OrderItemsController < ApplicationController
+  def create
+    @order = current_order
+    @product = @order.order_items.where(product_id: @order.find_existed_product(order_item_params)).first
+    if @product.nil?
+      @order.order_items.create(order_item_params)
+    else
+      @update_order = @order.order_items.where(product_id: @product.product_id).first.update_attributes(quantity: @product.quantity + order_item_params[:quantity].to_i)
+    end
+    @order.save
+    redirect_back(fallback_location: products_path)
+  end
+
+  def update
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.update_attributes(order_item_params)
+    @order_items = @order.order_items
+    redirect_back(fallback_location: cart_path)
+  end
+
+  def destroy
+    @order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.destroy
+    @order_items = @order.order_items
+    redirect_back(fallback_location: cart_path)
+  end
+private
+  def order_item_params
+    params.require(:order_item).permit(:quantity, :product_id)
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,28 @@
 class Order < ApplicationRecord
   belongs_to :order_status
+  has_many :order_items, dependent: :destroy
+  before_validation :set_order_status
+  before_save :update_subtotal
+
+  def subtotal
+    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+  end
+
+  def find_existed_product(item_params)
+    current_item = order_items.find { |item| item.product_id.to_s == item_params[:product_id] }
+    if current_item
+      item_params[:product_id]
+    else
+      item_params
+    end
+  end
+
+private
+  def set_order_status
+    self.order_status_id = 1 if self.order_status_id.nil?
+  end
+
+  def update_subtotal
+    self[:subtotal] = subtotal
+  end
 end
